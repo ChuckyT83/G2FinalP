a91f9ee35f26fdc79eb683ebb46553d19a31064f
ChuckyT83 <123396415+ChuckyT83@users.noreply.github.com>
2023-12-14 10:57:04 -0500

    push for cleaning code and adding comments

--- final1/polls/urls.py
+++ final1/polls/urls.py
@@ -4,9 +4,7 @@ from . import views
 app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:question_id>/", views.detail, name="detail"),
-    path("results/", views.results, name = "results"),
-    path("<int:question_id>/vote/", views.vote, name="vote"),
     path("compare/", views.compare, name="compare"),
-    path("survey/", views.survey, name="survey"),     
+    path("survey/", views.survey, name="survey"),
+    path("tryagain/", views.tryAgain, name="tryAgain"),     
 ]
--- final1/polls/views.py
+++ final1/polls/views.py
@@ -7,45 +7,11 @@ from .models import Character, Question, Answer, Choice
 def index(request):
     return render(request, "polls/index.html")
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    current_user = request.user
-    choice_id = current_user.id
-    choice = get_object_or_404(Choice, pk=choice_id)
-    if request.method=="POST":
-        answer = request.POST['answer']
-        if question_id == 1:
-            choice.choiceOne = answer
-        elif question_id == 2:
-            choice.choiceTwo = answer
-        elif question_id == 3:
-            choice.choiceThree = answer
-        elif question_id == 4:
-            choice.choiceFour = answer
-        print(answer)
-        choice.save()
-        try:
-            question_id = question_id + 1
-            question = Question.objects.get(pk=question_id)
-        except Question.DoesNotExist:
-            return compare(request)
-    return render(request, "polls/detail.html", {"question": question})
-
-def results(request):
-    question_list = Question.objects.order_by("id")
-    choice_list = Choice.objects.filter(user = 1).values()
-    questions = {"question_list": question_list}
-    choices = {"choice_list": choice_list}
-    print(choices)
-    print(choice_list)
-    return render(request, "polls/results.html", {"question_list": question_list, "choice_list": choice_list})
-
 def survey(request):
     current_user = request.user
     choice_id = current_user.id
     choice = get_object_or_404(Choice, pk=choice_id)
     questions = Question.objects.all()
-    print(questions)
     
     if request.method=="POST":
         try:
@@ -62,31 +28,25 @@ def survey(request):
             return compare(request)
         except:
             return render(request, "polls/tryagain.html")
+        
     return render(request, "polls/survey.html", {"questions": questions})
-
-def vote(request, question_id):
-    choice_id = 1
-    question = get_object_or_404(Question, pk=question_id)
-    choice = get_object_or_404(Choice, pk=choice_id)
-    answer = request.POST['answer']
-    choice.choiceOne = answer
-    return HttpResponse(request, "polls/index.html")
 
 def compare(request):
     current_user = request.user
     choice_id = current_user.id
     choices = Choice.objects.values_list("choiceOne","choiceTwo", "choiceThree", "choiceFour", "choiceFive", "choiceSix", "choiceSeven","choiceEight","choiceNine").get(id=choice_id)
-    print(choices)
     countList = []
-    for i in range(27):
+
+    for i in range(27): 
+        #itterates through the character list and compares each character to the choices made by the user
+        #then stores a count of each matching choice in a list.
         character_id = i+1
         characterTraits = Character.objects.values_list("human",'goodOrBad',"sex","support","range","weapons","otherOne","otherTwo","otherThree").get(id=character_id)
-        countList.append(sum(x==y for x, y in zip(choices, characterTraits)))
-        print(list(zip(choices, characterTraits)))
-        print(countList)
-    characterIndex = max(range(len(countList)), key=countList.__getitem__) + 1
-    print(characterIndex)
-    print(Character.objects.values_list("charName", flat = True).get(id=characterIndex))
-    charPick = Character.objects.values_list("charName", flat = True).get(id=characterIndex)
-    print(charPick)
+        countList.append(sum(x==y for x, y in zip(choices, characterTraits))) #compares each choice with character traits and gives a sum of total matches
+    characterIndex = max(range(len(countList)), key=countList.__getitem__) + 1 #finds the index of the highest match count in the comparison list
+    charPick = Character.objects.values_list("charName", flat = True).get(id=characterIndex) #pulls the character that matches the index
+
     return render(request, "polls/compare.html", {"charPick": charPick})
+
+def tryAgain(request):
+    return render(request, "polls/tryagain.html")
