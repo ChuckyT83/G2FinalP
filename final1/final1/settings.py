66d9cc99e2ec7045b2127ed084266ceba52e71e0
ChuckyT83 <123396415+ChuckyT83@users.noreply.github.com>
2023-12-15 15:09:16 -0500

    static push

--- final1/final1/settings.py
+++ final1/final1/settings.py
@@ -126,4 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = "/"
 
-STATIC_ROOT = "/home/Group2FinalP/polls/static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'polls/static')
